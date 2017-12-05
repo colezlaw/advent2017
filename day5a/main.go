cc0e344a9ee545ad603a33d1e0a4ed81047c2e3b
colezlaw <will@cltnc.us>
2017-12-05 07:23:20 -0500

    Completed Day 5 A

--- day5a/main.go
+++ day5a/main.go
@@ -1,5 +1,73 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+// EOP - End of Progran
+var EOP = errors.New("EOP")
+
+func step(program []int, pc int) (int, error) {
+	// First, check to see if the offset of pc is within the slice range
+	if pc < 0 || pc >= len(program) {
+		return 0, EOP
+	}
+
+	// We know the current pc is in range, so follow its instruction
+	newPC := pc + program[pc]
+
+	// Check to see if newPC is still within the program
+	if newPC < 0 || newPC >= len(program) {
+		return newPC, EOP
+	}
+
+	// Increment the previous instruction and return
+	program[pc]++
+
+	return newPC, nil
+}
+
+func count(program []int) int {
+	count := 0
+	var pc int
+	var err error
+	for {
+		fmt.Printf("PC is %d\n", pc)
+		pc, err = step(program, pc)
+		count++
+		if err == EOP {
+			break
+		}
+	}
+
+	return count
+}
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("error opening: %v", err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanLines)
+
+	var program []int
+	for s.Scan() {
+		n, err := strconv.Atoi(strings.Trim(s.Text(), " \n"))
+		if err != nil {
+			log.Fatalf("Unexpected error parsing %v", err)
+		}
+		program = append(program, n)
+	}
+
+	steps := count(program)
+	fmt.Println(steps)
 }
--- day5a/main_test.go
+++ day5a/main_test.go
@@ -1,7 +1,56 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestOne(t *testing.T) {
+	tt := []struct {
+		program  []int
+		pc       int
+		expected int
+		err      error
+	}{
+		{[]int{0, 3, 0, 1, -3}, 0, 0, nil},
+		{[]int{1, 3, 0, 1, -3}, 0, 1, nil},
+		{[]int{2, 3, 0, 1, -3}, 1, 4, nil},
+		{[]int{2, 4, 0, 1, -3}, 4, 1, nil},
+		{[]int{2, 4, 0, 1, -2}, 1, 0, EOP},
+	}
 
+	for x, tc := range tt {
+		t.Run(fmt.Sprintf("Instruction %d", x), func(t *testing.T) {
+			pc, err := step(tc.program, tc.pc)
+			if err != nil {
+				if err == tc.err {
+					return
+				}
+				t.Fatalf("Unexpected error %v", err)
+			}
+			if tc.expected != pc {
+				t.Errorf("Expected new PC to be %d, got %d", tc.expected, pc)
+			}
+		})
+	}
+}
+
+func TestProgram(t *testing.T) {
+	// Given a test program, just count the number of steps
+	// it takes to jump outside the program
+	tt := []struct {
+		program  []int
+		expected int
+	}{
+		{[]int{0, 3, 0, 1, -3}, 5},
+	}
+
+	for x, tc := range tt {
+		t.Run(fmt.Sprintf("Program %d", x), func(t *testing.T) {
+			actual := count(tc.program)
+			if actual != tc.expected {
+				t.Errorf("Expected to jump in %d steps, got %d", tc.expected, actual)
+			}
+		})
+	}
 }
