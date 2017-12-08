6065ebd10e83003dd2b6d0bf79a4d4bb54050713
colezlaw <will@cltnc.us>
2017-12-08 08:26:42 -0500

    8a

--- day8a/main.go
+++ day8a/main.go
@@ -1,5 +1,75 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+)
 
+func max(m map[string]int) int {
+	ret := int(math.MinInt32)
+	for _, v := range m {
+		if v > ret {
+			ret = v
+		}
+	}
+
+	return ret
+}
+
+func compute(input string, registers map[string]int) {
+	s := bufio.NewScanner(strings.NewReader(input))
+	s.Split(bufio.ScanLines)
+
+	for s.Scan() {
+		fields := strings.Fields(s.Text())
+
+		// It's conceivable that we could check against a register (which would return 0)
+		// but not explicitly set it - in which case maybe max could be negative, but it
+		// should be 0
+		if _, ok := registers[fields[4]]; !ok {
+			registers[fields[4]] = 0
+		}
+
+		incr := false
+		test, _ := strconv.Atoi(fields[6])
+		switch fields[5] {
+		case "<":
+			incr = registers[fields[4]] < test
+		case "<=":
+			incr = registers[fields[4]] <= test
+		case ">":
+			incr = registers[fields[4]] > test
+		case ">=":
+			incr = registers[fields[4]] >= test
+		case "!=":
+			incr = registers[fields[4]] != test
+		case "==":
+			incr = registers[fields[4]] == test
+		}
+		mul := 1
+		if fields[1] == "dec" {
+			mul = -1
+		}
+
+		if incr {
+			value, _ := strconv.Atoi(fields[2])
+			registers[fields[0]] += mul * value
+			fmt.Println(registers[fields[0]])
+		}
+	}
+}
+
+func main() {
+	buff, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	result := make(map[string]int)
+	compute(string(buff), result)
+	fmt.Println(max(result))
 }
--- day8a/main_test.go
+++ day8a/main_test.go
@@ -2,6 +2,44 @@ package main
 
 import "testing"
 
-func TestOne(t *testing.T) {
+var sample = `b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10`
 
+func TestSample(t *testing.T) {
+	expected := map[string]int{
+		"a": 1,
+		"b": 0,
+		"c": -10,
+	}
+
+	result := make(map[string]int)
+
+	compute(sample, result)
+	if len(expected) != len(result) {
+		t.Errorf("Expected len(result) to be %d, got %d", len(expected), len(result))
+	}
+
+	for k, v := range expected {
+		if v1, ok := result[k]; !ok {
+			t.Errorf("Expected result[%s] to be set, but it was not", k)
+		} else if v1 != v {
+			t.Errorf("Expected result[%s] to be %d, got %d", k, v, v1)
+		}
+	}
+}
+
+func TestMax(t *testing.T) {
+	expected := 1
+	input := map[string]int{
+		"a": 1,
+		"b": 0,
+		"c": -10,
+	}
+
+	actual := max(input)
+	if expected != actual {
+		t.Errorf("Expected max of %d, got %d", expected, actual)
+	}
 }
