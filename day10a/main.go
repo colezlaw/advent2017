2831b52eb584c7b8459e6d47093e10cd0422e6f4
colezlaw <will@cltnc.us>
2017-12-10 14:45:07 -0500

    Day 10a

--- day10a/main.go
+++ day10a/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import "fmt"
 
+type chain struct {
+	list    []int
+	pos     int
+	skip    int
+	lengths []int
+}
+
+func newChain(size int, lengths []int) *chain {
+	c := &chain{list: make([]int, size), lengths: lengths}
+
+	for i := 0; i < size; i++ {
+		c.list[i] = i
+	}
+
+	return c
+}
+
+func (c *chain) round() {
+	reverse(c.list, c.pos, c.lengths[c.skip])
+	c.pos = (c.pos + c.lengths[c.skip] + c.skip) % len(c.list)
+	c.skip++
+}
+
+func reverse(input []int, pos, l int) {
+	for i, j := 0, l-1; i < j; i, j = i+1, j-1 {
+		input[(pos+i)%len(input)], input[(pos+j)%len(input)] = input[(pos+j)%len(input)], input[(pos+i)%len(input)]
+	}
+}
+
+func (c *chain) hash() {
+	for c.skip < len(c.lengths) {
+		c.round()
+	}
+}
+
+func main() {
+	c := newChain(256, []int{120, 93, 0, 90, 5, 80, 129, 74, 1, 165, 204, 255, 254, 2, 50, 113})
+	c.hash()
+	fmt.Printf("%d\n", c.list[0]*c.list[1])
 }
--- day10a/main_test.go
+++ day10a/main_test.go
@@ -2,6 +2,55 @@ package main
 
 import "testing"
 
-func TestOne(t *testing.T) {
+func TestReverse(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    []int
+		pos      int
+		l        int
+		expected []int
+	}{
+		{"Simple", []int{1, 2, 3, 4}, 0, 4, []int{4, 3, 2, 1}},
+		{"Wrap", []int{1, 2, 3, 4}, 2, 4, []int{4, 3, 2, 1}},
+		{"Odd", []int{1, 2, 3, 4}, 0, 3, []int{3, 2, 1, 4}},
+		{"Round1", []int{0, 1, 2, 3, 4}, 0, 3, []int{2, 1, 0, 3, 4}},
+		{"Round2", []int{2, 1, 0, 3, 4}, 3, 4, []int{4, 3, 0, 1, 2}},
+		{"Round3", []int{4, 3, 0, 1, 2}, 3, 1, []int{4, 3, 0, 1, 2}},
+		{"Round4", []int{4, 3, 0, 1, 2}, 1, 5, []int{3, 4, 2, 1, 0}},
+	}
 
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			reverse(tc.input, tc.pos, tc.l)
+			if len(tc.input) != len(tc.expected) {
+				t.Fatalf("Expected len of %d, got %d", len(tc.expected), len(tc.input))
+			}
+			for k, v := range tc.input {
+				if v != tc.expected[k] {
+					t.Errorf("Expected index %d to be %d, was %d", k, tc.expected[k], v)
+				}
+			}
+		})
+	}
+}
+
+func TestRounds(t *testing.T) {
+	c := newChain(5, []int{3, 4, 1, 5})
+	expected := [][]int{
+		{2, 1, 0, 3, 4},
+		{4, 3, 0, 1, 2},
+		{4, 3, 0, 1, 2},
+		{3, 4, 2, 1, 0},
+	}
+	for i := range c.lengths {
+		c.round()
+		if c.skip != i+1 {
+			t.Fatalf("Expected skip to be %d, was %d", i+1, c.skip)
+		}
+		for k, v := range expected[i] {
+			if c.list[k] != v {
+				t.Errorf("Expected element %d to be %d, was %d", k, v, c.list[k])
+			}
+		}
+	}
 }
