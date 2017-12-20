236cfd5e8f56e2f9ea7b5e90ef2f75bab99550b2
colezlaw <will@cltnc.us>
2017-12-20 16:11:48 -0600

    Day 17 b

--- day17b/main.go
+++ day17b/main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
+type spinlock struct {
+	cbuf      int
+	index     int
+	count     int
+	steps     int
+	slot1     int
+	zeroindex int
+}
+
+func newSpinlock(steps int) *spinlock {
+	return &spinlock{cbuf: 1, steps: steps}
+}
+
+func (s *spinlock) spin() {
+	s.count++
+	s.index = (s.index+s.steps)%s.cbuf + 1
+	if s.index == 0 {
+		// We're inserting before element 0, so we shift the 0 index
+		s.zeroindex++
+	}
+	if s.index == s.zeroindex+1 {
+		fmt.Println(s.count)
+		s.slot1 = s.count
+	}
+	if s.index == 0 {
+		fmt.Println("yikes!")
+	}
+	s.cbuf++
+}
+
 func main() {
 
 }
--- day17b/main_test.go
+++ day17b/main_test.go
@@ -1,7 +1,14 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestOne(t *testing.T) {
-
+	sl := newSpinlock(382)
+	for i := 0; i < 50*1000*1000; i++ {
+		sl.spin()
+	}
+	fmt.Println(sl.slot1)
 }
