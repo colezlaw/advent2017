6813a2a7285d68b921aad8489cd75c168ff7683b
colezlaw <will@cltnc.us>
2017-12-20 14:40:03 -0600

    Day 16 a

--- day16a/main.go
+++ day16a/main.go
@@ -1,5 +1,94 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
+type scanner struct {
+	input string
+	start int
+	pos   int
+}
+
+func (s *scanner) token() (string, error) {
+	if s.pos >= len(s.input) {
+		return "", io.EOF
+	}
+	for ; s.pos < len(s.input); s.pos++ {
+		if s.input[s.pos] == ',' {
+			ret := s.input[s.start:s.pos]
+			s.pos = s.pos + 1
+			s.start = s.pos
+			return ret, nil
+		}
+	}
+	if s.start < s.pos {
+		return s.input[s.start:s.pos], nil
+	}
+	return "", io.EOF
+}
+
+func spin(s string, n int) string {
+	r := s[len(s)-n:] + s[0:len(s)-n]
+	return r
+}
+
+func partner(s string, a, b byte) string {
+	var n, o int
+	for i := 0; i < len(s); i++ {
+		if s[i] == a {
+			n = i
+		}
+		if s[i] == b {
+			o = i
+		}
+	}
+	return exchange(s, n, o)
+}
+
+func exchange(s string, n, o int) string {
+	r := []byte(s)
+	r[o], r[n] = r[n], r[o]
+	return string(r)
+}
+
+func operate(s, op string) string {
+	switch op[0] {
+	case 's':
+		n, _ := strconv.Atoi(op[1:])
+		return spin(s, n)
+	case 'x':
+		pieces := strings.Split(op[1:], "/")
+		n, _ := strconv.Atoi(pieces[0])
+		o, _ := strconv.Atoi(pieces[1])
+		return exchange(s, n, o)
+	case 'p':
+		return partner(s, op[1], op[3])
+	}
+
+	return s
+}
+
+func main() {
+	f, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	start := "abcdefghijklmnop"
+
+	s := scanner{input: string(f)}
+	for {
+		tok, err := s.token()
+		if err != nil {
+			break
+		}
+		start = operate(start, tok)
+	}
+
+	fmt.Println(start)
 }
--- day16a/main_test.go
+++ day16a/main_test.go
@@ -1,7 +1,70 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestOne(t *testing.T) {
+func TestSpin(t *testing.T) {
+	start := "abcde"
+	expected := "cdeab"
+	actual := spin(start, 3)
+	if actual != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, actual)
+	}
+}
 
+func TestExchange(t *testing.T) {
+	start := "eabcd"
+	expected := "eabdc"
+	actual := exchange(start, 3, 4)
+	if actual != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, actual)
+	}
+}
+
+func TestPartner(t *testing.T) {
+	start := "eabdc"
+	expected := "baedc"
+	actual := partner(start, 'b', 'e')
+	if actual != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, actual)
+	}
+}
+
+func TestOperate(t *testing.T) {
+	input := "abcde"
+	tt := []struct {
+		op       string
+		expected string
+	}{
+		{"s1", "eabcd"},
+		{"s3", "cdeab"},
+		{"x2/4", "abedc"},
+		{"pa/b", "bacde"},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.op, func(t *testing.T) {
+			r := operate(input, tc.op)
+			if r != tc.expected {
+				t.Errorf("Expected '%s', got '%s'", tc.expected, r)
+			}
+		})
+	}
+}
+
+func TestScanner(t *testing.T) {
+	s := scanner{input: "s1,x3/4,pb/e"}
+	expected := 3
+	count := 0
+	for {
+		_, err := s.token()
+		if err != nil {
+			break
+		}
+		count++
+	}
+	if count != expected {
+		t.Errorf("Expected %d, got %d", expected, count)
+	}
 }
