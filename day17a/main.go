093f6a44f94e1053e8c68844c6a43f5a2bcb3a29
colezlaw <will@cltnc.us>
2017-12-20 16:11:39 -0600

    Day 17 a

--- day17a/main.go
+++ day17a/main.go
@@ -1,5 +1,28 @@
 package main
 
+type spinlock struct {
+	cbuf  []int
+	index int
+	count int
+	steps int
+}
+
+func newSpinlock(steps int) *spinlock {
+	return &spinlock{cbuf: []int{0}, steps: steps}
+}
+
+func (s *spinlock) spin() {
+	s.count++
+	nbuf := make([]int, len(s.cbuf)+1)
+	s.index = (s.index + s.steps + 1) % len(s.cbuf)
+	if s.index > 0 {
+		copy(nbuf, s.cbuf[0:s.index])
+	}
+	nbuf[s.index] = s.count
+	copy(nbuf[s.index+1:], s.cbuf[s.index:])
+	s.cbuf = nbuf
+}
+
 func main() {
 
 }
--- day17a/main_test.go
+++ day17a/main_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestOne(t *testing.T) {
+	sl := newSpinlock(3)
+	for i := 0; i < 10; i++ {
+		sl.spin()
+	}
 
+	for i := 0; i < len(sl.cbuf); i++ {
+		if sl.cbuf[i] == 2017 {
+			fmt.Println(sl.cbuf[i+1])
+		}
+	}
 }
